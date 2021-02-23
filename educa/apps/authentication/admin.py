dc3f01b99d734e68dfa2a1654953973cbce98ce1
Ibra <ibragimgiyazov@gmail.com>
2021-02-23 15:45:33 +0300

    profile admin

--- educa/apps/authentication/admin.py
+++ educa/apps/authentication/admin.py
@@ -3,7 +3,7 @@ from .models import Profile
 
 @admin.register(Profile)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['username', 'company_name', 'photo']
+    list_display = ['username', 'company_name', 'photo', 'promocode', 'bonus', 'bill_number', 'geo', 'site']
     list_filter = ['username']
     search_fields = ['username', 'company_name']
     # prepopulated_fields = {'slug': ('title',)}

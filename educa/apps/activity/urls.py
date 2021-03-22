54c02d08431ab7142ea904fd7fffc184712f3563
Ibra <ibragimgiyazov@gmail.com>
2021-03-22 11:17:47 +0300

    delete all slashes on end of urls

--- educa/apps/activity/urls.py
+++ educa/apps/activity/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('like/', add_like_course, name='course_like'),
-    path('comment/', CommentDetailView.as_view(), name='add_comment'),
-    path('comments/', CommentListView.as_view(), name='all_comments'),
+    path('like', add_like_course, name='course_like'),
+    path('comment', CommentDetailView.as_view(), name='add_comment'),
+    path('comments', CommentListView.as_view(), name='all_comments'),
 ]
--- educa/apps/courses/urls.py
+++ educa/apps/courses/urls.py
@@ -9,19 +9,19 @@ from .views import (CategoryListView,
                     )
 
 urlpatterns = [
-    path('categories/', CategoryListView.as_view(), name='category_list'),
+    path('categories', CategoryListView.as_view(), name='category_list'),
     path('subcategories/<int:id>', SubcategoryListView.as_view(), name='subcategory_list'),
     path('courses/<slug:category>', CourseListView.as_view(), name='course_list'),
     path('courses/<slug:category>/<slug:subcategory>', CourseListView.as_view(), name='course_list_sub'),
-    path('courses/', CourseListView.as_view(), name='all_course_list'),
+    path('courses', CourseListView.as_view(), name='all_course_list'),
     path('course/<int:pk>', CourseDetailView.as_view(), name='course_detail'),
     path('course/create', CourseDetailView.as_view(), name='course_create'),
     path('course/save', course_save, name='save_course'),
     path('lessons/<int:course_id>', LessonListView.as_view(), name='lesson_list'),
-    path('lessons/', LessonListView.as_view(), name='lesson_all_list'),
+    path('lessons', LessonListView.as_view(), name='lesson_all_list'),
     path('lesson/<int:id>', LessonDetailView.as_view(), name='lesson_detail'),
     path('lesson/create', LessonDetailView.as_view(), name='lesson_create'),
-    path('levels/', LevelListView.as_view(), name='level_list'),
+    path('levels', LevelListView.as_view(), name='level_list'),
     path('tmp/create', tmp, name='tmp_create'),
     path('tmp/delete', tmp, name='tmp_delete'),
 ]

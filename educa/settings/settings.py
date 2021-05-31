373190183709bda05ead781e24f230948627facc
Ibra <ibragimgiyazov@gmail.com>
2021-05-31 14:39:00 +0300

    set ssl 2

--- educa/settings/settings.py
+++ educa/settings/settings.py
@@ -6,9 +6,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 SECRET_KEY = os.getenv('SECRET_KEY')
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['84.252.134.196']
+ALLOWED_HOSTS = ['api.youcleve.com']
 # ALLOWED_HOSTS = ['*']
 
 

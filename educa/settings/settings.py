45edd7ec7020fb352866064bf87a61f14d600425
Ibra <ibragimgiyazov@gmail.com>
2021-06-02 12:42:45 +0300

    Debug true

--- educa/settings/settings.py
+++ educa/settings/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 SECRET_KEY = os.getenv('SECRET_KEY')
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['api.youcleve.com']
 # ALLOWED_HOSTS = ['*']
